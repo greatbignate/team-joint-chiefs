d04f686687a70ee519879ab0bea70b60546676d1
Joey Eisenzimmer <joey.eisenzimmer@gmail.com>
2018-08-29 14:32:38 -0700

    worked through functions, will add craps and pit boss later

--- js/print.js
+++ js/print.js
@@ -20,6 +20,7 @@ var roulette = 'roulette';
 var skirts = 'skirts';
 var accessories = 'accessories';
 var pitboss = 'pitboss';
+var sixBySix = 'sixbysix';
 
 loadSheet = JSON.parse(localStorage.getItem('loadSheet'));
 
@@ -98,7 +99,7 @@ function renderTableType() {
       buildTables(loadSheet[i].pokerTray, loadSheet[i].name, tables);
     }
     else if (loadSheet[i].name === 'Craps') {
-      buildTables(loadSheet[i].tableSize, loadSheet[i].name, tables); // mostly working, just not displaying table size
+      buildTables(loadSheet[i].tablesize, loadSheet[i].name, tables); // mostly working, just not displaying table size
     }
   }
 }
@@ -110,33 +111,61 @@ renderTableType();
 function renderRingType() {
 // var tableEl = document.getElementById('rings');
 //  {
+  if (subTotals[4] !== 0) {
+    buildTables(subTotals[4], 'D-Ring Regular', rings);
+
+    // var trEl = document.createElement('tr');
+    // var trEl = document.createElement('td');
+    // tdEl.textContent = subTotals[4];
+    // trEl.appendChild(tdEl);
+    // tdEl = document.createElement('td');
+    // tdEl.textContent = ' ';
+    // trEl.appendChild(tdEl);
+    // tdEl = document.createElement('td');
+    // tdEl.textContent = 'D-Ring Regular';
+    // trEl.appendChild(tdEl);
+
+    // tableEl.appendChild(trEl);
+  }
   for (var i = 0; i < loadSheet.length; i++) {
-    if (subTotals[4] !== 0) {
-      buildTables(subTotals[4], 'D-Ring Regular', rings);
-
-      // var trEl = document.createElement('tr');
-      // var trEl = document.createElement('td');
-      // tdEl.textContent = subTotals[4];
-      // trEl.appendChild(tdEl);
-      // tdEl = document.createElement('td');
-      // tdEl.textContent = ' ';
-      // trEl.appendChild(tdEl);
-      // tdEl = document.createElement('td');
-      // tdEl.textContent = 'D-Ring Regular';
-      // trEl.appendChild(tdEl);
-
-      // tableEl.appendChild(trEl);
-    }
+    if (subTotals[i].roulleteSkirt);
+    buildTables(loadSheet[i].tray, loadSheet[i].tableSize + 'Roulette Ring', rings);
   }
 
 }
 
-function renderTrayType() {
+function renderSixBySix() {
+  if (subTotals[0] !== 0) {
+    buildTables(subTotals[0], 'Six By Six', sixBySix);
+  }
+}
 
+function renderTrayType() {
+  if (subTotals[2] !== 0) {
+    buildTables(subTotals[2], 'Regular Trays', trays);
+  }
+  for (var i = 0; i < loadSheet.length; i++) {
+    if (loadSheet[i].celebTray) {
+      buildTables(loadSheet[i].celebTray, 'Celebrity Trays', trays);
+    }
+  }
+  for (var i = 0; i < loadSheet.length; i++) {
+    if (loadSheet[i].pokerTray) {
+      buildTables(loadSheet[i].pokerTray, 'Poker Tray', trays);
+    }
+  }
 }
 
 function renderDealerItems() {
-
+  var dealerCount = 0;
+  for (var i = 0; i < loadSheet.length; i++) {
+    dealerCount += loadSheet[i].dealer;
+  }
+  buildTables(dealerCount, 'Tux Shirts', dealers);
+  buildTables(dealerCount, 'Set-up T-Shirts', dealers);
+  buildTables(dealerCount, 'Stud Kits', dealers);
+  buildTables(dealerCount, 'Bow Ties', dealers);
+  buildTables(dealerCount, 'Cummerbunds', dealers);
 }
 
 function renderCrapsAll() {
@@ -144,15 +173,51 @@ function renderCrapsAll() {
 }
 
 function renderRouletteItems() {
-
+  for (var i = 0; i < loadSheet.length; i++) {
+    if (loadSheet[i].wheelSize) {
+      buildTables(loadSheet[i].wheelSize, 'Roulette Wheel', roulette);
+      buildTables(2, 'Markers', roulette);
+      buildTables(2, 'Balls', roulette);
+      buildTables(2, 'Pay Sheets', roulette);
+    }
+  }
 }
 
 function renderSkirtsType() {
+  if (subTotals[1] !== 0) {
+    buildTables(subTotals[1], 'Regular Skirts', skirts);
+  }
+  for (var i = 0; i < loadSheet.length; i++) {
+    if (loadSheet[i].rouletteSkirt) {
+      buildTables(loadSheet[i].rouletteSkirt, 'Roulette Skirt', skirts);
+    }
+  }
 
 }
 
 function renderAccessories() {
+  for (var i = 0; i < loadSheet.length; i++) {
+    if (loadSheet[i].celebSign) {
+      buildTables(loadSheet[i].celebSign, 'Celebrity Signs', accessories);
+    }
+  }
+  buildTables(1, 'Raffle Drum', accessories);
+  buildTables('??', '30,000 Chits', accessories);
 
+  var coasters = (subTotals[0] * 6 + subTotals[2]) * 12; // refactor this - BRUTE FORCE
+  buildTables(coasters, 'Coasters', accessories);
+
+  buildTables('??', 'Ticket Bags', accessories); // refactor this
+  buildTables(subTotals[0] * 6 + subTotals[2], 'Dealer Towels', accessories);
+  buildTables(1, 'Hand Truck', accessories);
+  
+  for (var i = 0; i < loadSheet.length; i++) {
+    if (loadSheet[i].button) {
+      buildTables(loadSheet[i].button, 'Dealer Button', accessories);
+      buildTables(loadSheet[i].cusion, 'Dealer Cushion', accessories);
+      buildTables(loadSheet[i].chairs, 'Chairs', accessories); // may require if statement
+    }
+  }
 }
 
 function renderPitBoss() {
